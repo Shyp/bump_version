ee9cfb92db5f46c65591f4a32cd610810c30d503
Kevin Burke <burke@shyp.com>
2016-02-09 20:40:10 -0800

    0.2

--- bump_version/main.go
+++ bump_version/main.go
@@ -10,7 +10,7 @@ import (
 	"github.com/Shyp/bump_version"
 )
 
-const VERSION = "0.1"
+const VERSION = "0.2"
 
 func usage() {
 	fmt.Fprintf(os.Stderr, "Usage: bump_version <major|minor|patch> <filename>\n")
